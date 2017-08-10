6b162076582360c0b5051a7dd29231bd5b7deee5
Eric Sanner <eric.sanner@perficient.com>
2017-08-10 09:07:54 -0400

    Home Page, Index, Styles

--- Book2017/Controllers/UsaCjjController.cs
+++ Book2017/Controllers/UsaCjjController.cs
@@ -7,52 +7,60 @@ using System.Web.Mvc;
 
 namespace Book2017.Controllers
 {
-	public class UsaCjjController : GlassController
-    {
-        public ActionResult Navigation()
-        {
-            Item itm = SitecoreContext.GetHomeItem<Item>();
-            Navigation model = new Navigation();
+    public class UsaCjjController : GlassController
+	{
+		public ActionResult Navigation()
+		{
+			Item itm = SitecoreContext.GetHomeItem<Item>();
+			Navigation model = new Navigation();
 
-            model.Pages = itm.GetChildren();
+			model.Pages = itm.GetChildren();
 
-            return View("~/Views/UsaCjj/Navigation.cshtml", model);
-        }
-        public ActionResult Section()
-        {
-            Item itm = SitecoreContext.GetCurrentItem<Item>();
-            ISection model = SitecoreContext.GetCurrentItem<ISection>();
+			return View("~/Views/UsaCjj/Navigation.cshtml", model);
+		}
 
-            model.Pages = itm.GetChildren();
-            
-            return View("~/Views/UsaCjj/Section.cshtml", model);
-        }
+		public ActionResult HomePage()
+		{
+			IHomePage model = SitecoreContext.GetCurrentItem<IHomePage>();
 
-        public ActionResult Content()
-        {
-            IContent model = SitecoreContext.GetCurrentItem<IContent>();
-            model.Parent = SitecoreContext.GetCurrentItem<Item>().Parent;
+			return View("~/Views/UsaCjj/HomePage.cshtml", model);
+		}
 
-            return View("~/Views/UsaCjj/Content.cshtml", model);
-        }
+		public ActionResult Section()
+		{
+			Item itm = SitecoreContext.GetCurrentItem<Item>();
+			ISection model = SitecoreContext.GetCurrentItem<ISection>();
 
-        public ActionResult Technique()
-        {
-            ITechnique model = SitecoreContext.GetCurrentItem<ITechnique>();
-            model.Parent = SitecoreContext.GetCurrentItem<Item>().Parent;
+			model.Pages = itm.GetChildren();
+			
+			return View("~/Views/UsaCjj/Section.cshtml", model);
+		}
 
-            return View("~/Views/UsaCjj/Technique.cshtml", model);
-        }
+		public ActionResult Content()
+		{
+			IContent model = SitecoreContext.GetCurrentItem<IContent>();
+			model.Parent = SitecoreContext.GetCurrentItem<Item>().Parent;
 
-        public ActionResult IndexAlphabetical()
-        {
-	        Sitecore.Data.Database database = Sitecore.Configuration.Factory.GetDatabase("web");
-	        List<Item> allItems = database.SelectItems("/sitecore/Content/Home/*/*").ToList();
+			return View("~/Views/UsaCjj/Content.cshtml", model);
+		}
+
+		public ActionResult Technique()
+		{
+			ITechnique model = SitecoreContext.GetCurrentItem<ITechnique>();
+			model.Parent = SitecoreContext.GetCurrentItem<Item>().Parent;
+
+			return View("~/Views/UsaCjj/Technique.cshtml", model);
+		}
+
+		public ActionResult IndexAlphabetical()
+		{
+			Sitecore.Data.Database database = Sitecore.Configuration.Factory.GetDatabase("web");
+			List<Item> allItems = database.SelectItems("/sitecore/Content/Home/*/*").ToList();
 
 			//sort
 			//remove index pages
 			
 			return View("~/Views/UsaCjj/IndexAlphabetical.cshtml", allItems);
-        }
-    }
+		}
+	}
 }
